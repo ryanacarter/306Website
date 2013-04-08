8642f525cd5f7aad1d18a1af3f0797f20ce7b0e1
ryanacarter <ryanacarter@comcast.net>
2013-04-08 16:47:05 -0400

    Fixed the styling on the main page. It now will adjust itself to the size of the browser.

--- main.php
+++ main.php
@@ -26,67 +26,60 @@
   <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
 </head>
 <body>
-	<div class="container clearfix">
+	<div class="header">
 		<div class="grid_12">
-        <img  class="banner" src="img/Banner3.gif" alt="DukeServe">
+        	<img  class="banner" src="img/Banner3.gif" alt="DukeServe">
+		</div>
     </div>
   
-    <div class="grid_12">
-      <?php echo '<h1>Student Name: ' . $firstName . ' ' . $lastName . '</h1>' ?>
-      	<img border="1" src="<?php echo $picture ?>" alt="<?php echo $firstName . ' ' . $lastName?> " 		height="230px" width="175px" style=" float:right; border-color:#000000;">
+    <div class="grid_11">
+		<div class="grid_10">
+      		<?php echo 'Student Name: ' . $firstName . ' ' . $lastName ?>
+		</div>
+      		<img border="1" src="<?php echo $picture ?>" alt="<?php echo $firstName .
+			' ' . $lastName?> "height="200px" width="150px" 
+			style=" float:right; border-color:#000000;">
+		</div>
     </div>
-    <div>
+    <div class="grid_9">
        <a href="punch.php?rfid=<?php echo $rfid ?>" class="button">
-        <button type="button" style="border: 0; background: transparent;
-	position: relative; bottom:200px;">
+        <button type="button" style="border: 0; background: transparent;">
           <img src="img/Punch.gif" width="600" alt="submit" />
         </button>
       </a>
 
       <a href="dining.php?rfid=<?php echo $rfid ?>" class="button">
-        <button type="button" style="border: 0; background: transparent;
-	position: relative; bottom:200px;">
+        <button type="button" style="border: 0; background: transparent;">
         <img src="img/Dining.gif" width="600" alt="submit" />
         </button>
       </a>
 
       <a href="flex.php?rfid=<?php echo $rfid ?>" class="button">
-        <button type="button" style="border: 0; background: transparent;
-	position: relative; bottom:200px;">
+        <button type="button" style="border: 0; background: transparent;">
           <img src="img/Flex.gif" width="600" alt="submit" />
         </button>
       </a>
     </div>
 
-    <div class="grid_3" id="balance">
-      <a href="balance.php?rfid=<?php echo $rfid ?>" class="button" id="balance">
-        <button type="button" style="border: 0; background: transparent;
-	position: relative; bottom:200px;">
-          <img src="img/Balance.gif" width="275" alt="submit" />
-        </button>
-      </a>
+    <div class="grid_8">
+		<div class="grid_7_left">
+      		<a href="balance.php?rfid=<?php echo $rfid ?>" class="button" id="balance">
+        		<button type="button" style="border: 0; background: transparent; float: left;">
+          			<img src="img/Balance.gif" width="275" alt="submit" />
+        		</button>
+      		</a>
+		</div>
+		<div class="grid_7_right">
+      		<a href="index.php" class="button" id="balance">
+        		<button type="button" style="border: 0; background: transparent; float: right;">
+					<img src="img/Quit.gif" width="275" alt="submit" />
+        		</button>
+      		</a>
+		</div>
     </div>
-    <div class="grid_3 omega">
-      <a href="index.php" class="button" id="balance">
-        <button type="button" style="border: 0; background: transparent;
-			position: relative; bottom:200px;">
-		<img src="img/Quit.gif" width="275" alt="submit" />
-        </button>
-      </a>
-    </div>
-    <!--------<div class="grid_4">
-      <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      </p>
-    </div>
-
-    <div class="grid_8 omega">
-      <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      </p>
-    </div>-->
-
-
-
-	</div>
+	
+	<div class="footer"><br></div>
+	
 </body>
 </html>
   
